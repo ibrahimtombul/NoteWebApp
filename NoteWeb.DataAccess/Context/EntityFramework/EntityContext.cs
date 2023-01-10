8c4bfb27ac873feb9b2dd3048dea911bae138a79
ibrahim <sawkopat@gmail.com>
2023-01-10 17:45:18 +0300

    1

---
+++ NoteWeb.DataAccess/Context/EntityFramework/EntityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteWeb.DataAccess.Context.EntityFramework
+{
+    public class EntityContext : DbContext
+    {
+
+
+        public EntityContext(DbContextOptions<EntityContext> options):base(options)
+        {
+
+        }
+    }
+}
